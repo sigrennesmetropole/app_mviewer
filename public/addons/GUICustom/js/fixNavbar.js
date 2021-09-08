ae7e24562111a168b62b930b06c634bf0423ad9f
DAVID Benoît <b.david@mutu.local>
2021-09-08 16:06:32 +0200

    fix erreur de changement de taille de police pour les titres de grande taille

--- public/addons/GUICustom/js/fixNavbar.js
+++ public/addons/GUICustom/js/fixNavbar.js
@@ -1,12 +1,10 @@
-// textLength = 85;
 if(screen.width <= 767){
   console.log($('#mv-navbar a.mv-title').text().length);
   if($('#mv-navbar a.mv-title').text().length > 22){
-  // if(textLength > 22){
     $('#mv-navbar a.mv-title').css({'cssText': 'font-size: 15px;max-width: 250px;top:-10px;position:relative;'});
-    if(textLength > 50){
+    if($('#mv-navbar a.mv-title').text().length > 50){
       $('#mv-navbar a.mv-title').css({'cssText': 'font-size: 10px;max-width: 250px;top:-10px;position:relative;'});
-      if(textLength > 80){
+      if($('#mv-navbar a.mv-title').text().length > 80){
         //can add 156 characters on the map title
         $('#mv-navbar a.mv-title').css({'cssText': 'font-size: 5px;max-width: 250px;top:-10px;position:relative;'});
       }
