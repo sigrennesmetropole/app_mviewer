506801ddfa4c9f62841224b8e7bb4ef207a11207
DAVID Benoît <b.david@mutu.local>
2021-12-07 15:34:18 +0100

    mise en place du panneau de droite pour les piscines

--- site_internet/customlayer/piscinesRM.js
+++ site_internet/customlayer/piscinesRM.js
@@ -1,16 +1,16 @@
 
 mviewer.customLayers.piscinesRM= (function() {
-    
+
     // TODO : cacher la clé d'API dans un fihcier de conf
     var apiKey = 'c583383089f1c7e544e32cdf44c11045';
-    
+
     let data_site = 'https://public.sig.rennesmetropole.fr/geoserver/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=v_sitorg_site&outputFormat=application%2Fjson&srsname=EPSG:3857&CQL_FILTER=id_specialite_principale=95';
-    
-    
-    /******************************** 
+
+
+    /********************************
     * ***        DATA             ***
     ********************************/
-    
+
     /**
     * Requete http sans header (WFS geoserver)
     **/
@@ -23,9 +23,9 @@ mviewer.customLayers.piscinesRM= (function() {
                 resolve({'response': res, 'category': 'geoserver'});
             });
         });
-        
+
     }
-    
+
     /**
     * Requete http avec header (API S&O qui nécessite une clé en header)
     **/
@@ -40,7 +40,7 @@ mviewer.customLayers.piscinesRM= (function() {
             });
         });
     };
-    
+
     /**
     * Données agglomérées des piscines et de leurs bassins
     **/
@@ -57,43 +57,73 @@ mviewer.customLayers.piscinesRM= (function() {
                     sitesorgs_data('https://api-sitesorg.sig.rennesmetropole.fr/v1/organismes/' + piscines_org[org].idOrganisme.idOrganisme, piscines_org[org].flagOrganismePrincipal).then(function (result_org, siteid=site_data.getId()) {
                         let org_detail = result_org.response;
                         updateFeature(result_org);
+
                     });
-                
+
                 }
             });
         }
     }
-    
+
     function updateFeature(org_data) {
         // Recherche de la feature concernée par l'organisme
         var feature = getFeatureFromIdSite(org_data.response.sites[0].idSite.idSite);
         org_data.response.horairesOuvertures = cleanedHoraires(org_data.response.horairesOuvertures);
-        
+
         if (feature != undefined && org_data.org_principal){
             // organisme principal = données principales de la piscine
             feature.set('idOrganisme', org_data.response.idOrganisme);
             feature.set('nomUsage', org_data.response.nomUsage);
             feature.set('horairesOuvertures', org_data.response.horairesOuvertures);
             feature.set('joursFermes', org_data.response.joursFermes);
+            console.log(feature);
+            console.log(feature.values_.nom_site);
+            var content = feature.values_.horairesOuvertures;
+            content.forEach((periode, i) => {
+              var testedHoraires = 0;
+              periode.horaires.forEach((horaire, i) => {
+                if(horaire.ouvert1 != null){
+                  testedHoraires++;
+                }
+              });
+              if(testedHoraires === 0) {
+                delete periode.horaires;
+              };
+
+            });
+
         } else{
-            // organisme secondaire = bassin 
+            // organisme secondaire = bassin
             feature.get('bassins').push(org_data.response);
+            if (feature != undefined && org_data.response){
+              // console.log(feature);
+              var bassins = feature.values_.bassins;
+              bassins.forEach((bassin, i) => {
+                if(bassin.horairesOuvertures){
+                  bassin.horairesOuvertureBassins = bassin.horairesOuvertures;
+                  if(bassin.horairesOuvertures.length === 0){
+                    bassin.horairesOuvertureBassins = null;
+                  };
+                  delete bassin.horairesOuvertures;
+                }
+              });
+            }
         }
     }
-    
+
     function getFeatureFromIdSite(idSite) {
         var features = layer.getSource().getFeatures();
         var retour;
         for (feat in features){
             if(idSite == features[feat].get('id_site')) {
-                retour = features[feat]; 
+                retour = features[feat];
                 break;
             }
         };
         return retour;
     }
-    
-    
+
+
     function cleanedHoraires(horairesColl){
         var valide = [];
         var refDebut;
@@ -106,7 +136,7 @@ mviewer.customLayers.piscinesRM= (function() {
                 } else if (refDebut == undefined || refDebut == null || refDebut < debut) {
                     valide.push(horairesColl[i]);
                     refDebut = debut;
-                } 
+                }
             }
         }
         // consever uniquement les horaires dont début >= refDebut
@@ -116,12 +146,19 @@ mviewer.customLayers.piscinesRM= (function() {
                 l_cleaned.push(valide[j]);
             }
         }
-        
+
         l_cleaned.sort(compareHoraires);
-        
+        l_cleaned.forEach((item, i) => {
+          if(i<l_cleaned.length-1){
+            if(item.dateFin === null && Date.parse(item.dateDebut) < Date.parse(l_cleaned[i+1].dateDebut)){
+              item.dateFin = l_cleaned[i+1].dateDebut;
+            }
+          }
+        });
+
         return l_cleaned;
     }
-    
+
     function compareHoraires( horA, horB ) {
        debA = new Date(horA.dateDebut);
        debB = new Date(horB.dateDebut);
@@ -133,10 +170,10 @@ mviewer.customLayers.piscinesRM= (function() {
       }
       return 0;
     }
-    
-    
-    
-    /******************************** 
+
+
+
+    /********************************
     * ***     REPRESENTATION      ***
     ********************************/
     function markerstyle() {
@@ -151,10 +188,10 @@ mviewer.customLayers.piscinesRM= (function() {
               });
         return [style];
     }
-    
-    
 
-    
+
+
+
     let layer = new ol.layer.Vector({
         source: new ol.source.Vector({
             format: new ol.format.GeoJSON(),
@@ -162,9 +199,9 @@ mviewer.customLayers.piscinesRM= (function() {
         }),
         style: markerstyle,
     });
-    
-    
-    
+
+
+
     layer.getSource().once('change',() =>{
         getSiteData();
         /*
@@ -173,12 +210,8 @@ mviewer.customLayers.piscinesRM= (function() {
         });
         */
     });
-    
 
-    
     return {
         layer: layer,
     }
 }());
-
-
