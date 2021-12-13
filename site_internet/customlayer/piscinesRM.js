38ef152ab0e183a9fba6a22b998a45e2ff509a8a
DAVID Benoît <b.david@mutu.local>
2021-12-13 11:39:11 +0100

    cleanup console.log

--- site_internet/customlayer/piscinesRM.js
+++ site_internet/customlayer/piscinesRM.js
@@ -83,8 +83,8 @@ mviewer.customLayers.piscinesRM= (function() {
             feature.set('adresse_postale_cedex', org_data.response.sites[0].adressePostaleCedex);
             feature.set('horairesOuvertures', org_data.response.horairesOuvertures);
             feature.set('joursFermes', org_data.response.joursFermes);
-            console.log(feature.values_.nom_site);
-            console.log(feature);
+            // console.log(feature.values_.nom_site);
+            // console.log(feature);
             var content = feature.values_.horairesOuvertures;
             content.forEach((periode, i) => {
               var testedHoraires = 0;
