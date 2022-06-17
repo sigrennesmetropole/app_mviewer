e11aa1f671b26e86da5d71a1476622d165128cd0
DAVID Benoît <b.david@mutu.local>
2022-06-17 16:55:27 +0200

    correctif onglets et leur couleur

--- public/addons/GUICustom/js/navTabs.js
+++ public/addons/GUICustom/js/navTabs.js
@@ -1,18 +1,36 @@
-confColors = configuration.getConfiguration().themes.theme[0].layer;
+confColors = configuration.getConfiguration().themes.theme; //[0].layer;
 $(document).on('click', '#map', function (e) {
-  console.log(confColors);
-
 
   setTimeout(function(){
     var navTabChilds = $('.nav-tabs')[0].childNodes;
 
     navTabChilds.forEach(function(child){
-      child.children[0].style.backgroundColor = '#4D4D4D';
-      confColors.forEach((confColor, i) => {
 
-        if(mviewer.getLayer(child.getAttribute("data-layerid")).name === confColor.name){
-          child.children[0].style.backgroundColor = confColor.tabColor;
-        }
+      confColors.forEach((themes, i) => {
+        themes.layer.forEach((confColor, i) => {
+
+          if(mviewer.getLayer(child.getAttribute("data-layerid")).name === confColor.name){
+
+            if(child.className === 'active'){
+              child.children[0].style.backgroundColor = confColor.tabColor || '#000000';
+              child.children[0].children[0].style.color = '#ffffff';
+            }else{
+              child.children[0].style.backgroundColor = '#ffffff';
+              child.children[0].children[0].style.color = '#000000';
+              child.children[0].children[0].style.opacity = '0.55';
+            }
+
+            if(confColor.tabColor){
+              child.children[0].children[0].style.color = confColor.tabColor;
+            }
+
+            if(child.className === 'active' && confColor.tabColor){
+              child.children[0].children[0].style.color = '#ffffff';
+            }
+
+          }
+
+        });
 
       });
     });
