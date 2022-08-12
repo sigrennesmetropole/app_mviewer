4dadc07922d1bf1bcad6f36983d7a77a4a29b980
ronanren <renouxpro@gmail.com>
2022-08-12 16:55:34 +0200

    ajout customlayer dynamique

--- balades/customlayer/balades.js
+++ balades/customlayer/balades.js
@@ -1,19 +1,31 @@
 
 mviewer.customLayers.balades = (function() {
     
-    var data_bal = 'apps/balades/customlayer/data/balades_3857.geojson';
-    
-    /* 
-    // style d'une couche surfacique
-    function baladeStyle_surf() {
-        let style = new ol.style.Style({
-                fill: new ol.style.Fill({
-                  color: '#d485c5', 
-                }),
-              });
-        return [style];
+    var data_bal;
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", this.API.config, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                parser = new DOMParser();
+                xmlDoc = parser.parseFromString(allText,"text/xml");
+                var layers = xmlDoc.getElementsByTagName("layer");
+                var i = 0;
+                while (i < layers.length){
+                    if (layers[i].getAttribute('url') === "apps/balades/customlayer/balades.js"){
+                        data_bal = layers[i].getAttribute('geojson');
+                        break;
+                    }
+                    i++;
+                }
+            }
+        }
     }
-    */
+    rawFile.send(null);
     
     function baladeStyle_lin() {
     // style d'une couche linéaire
