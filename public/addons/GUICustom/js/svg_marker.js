1a54622194a00b1c277208ba1b955a217e87b6f8
Cédric Briand <ce.briand@rennesmetropole.fr>
2022-08-24 15:44:02 +0200

    retrait des modifications RM du core

---
+++ public/addons/GUICustom/js/svg_marker.js
@@ -0,0 +1,9 @@
+
+/*
+* Personnalisation du marqueur de position
+*/
+var rmMarker = '<svg id="mv_marker" class="mv_marker_svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 60 87.6" style="enable-background:new 0 0 60 87.6;" xml:space="preserve" width="30px" height="30px"><style type="text/css">.st0{fill:#000000;}</style><path class="st0" d="M30.6,85.4C22.3,71,13.9,57.2,6.4,42.9C1,32.8,2.5,22.7,9.8,13.9c7.3-8.8,17-11.8,28-8.8 C48.5,8,55,15.5,57.2,26.5c1.2,5.8,0.4,11.5-2.4,16.6C46.9,57.1,38.8,71,30.6,85.4z M30.6,12.4c-9.2-0.1-16.9,7.6-17.1,16.9 c-0.2,9.2,7.5,17.1,17,17.3c9.1,0.2,17.1-7.6,17.2-16.8C47.8,20.3,40.1,12.5,30.6,12.4z"/></svg>';
+
+
+$("svg#mv_marker").replaceWith(rmMarker);
+
