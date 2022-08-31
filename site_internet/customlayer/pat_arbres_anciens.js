30a6d31617e987c742dc04b3a203ba8f14b507ae
Cédric Briand <38095778+cbriand@users.noreply.github.com>
2022-08-31 12:33:13 +0200

    message complémentaire sur age de l'arbre ancien (#130)

--- site_internet/customlayer/pat_arbres_anciens.js
+++ site_internet/customlayer/pat_arbres_anciens.js
@@ -1,5 +1,6 @@
 mviewer.customLayers.arbresanciens= (function() {
-    let dateref = new Date().getFullYear()-50;
+    let age_ancien = 50;
+    let dateref = new Date().getFullYear() - age_ancien;
     let data_url = 'https://public.sig.rennesmetropole.fr/geoserver/wfs?service=WFS&version=1.0.0&request=GetFeature&typeNames=espub_esv:gev_aali&CQL_FILTER=date_plant<'+ dateref +'&outputFormat=application/json&srsName=EPSG:4326';
     let datacolor = '#628A31'
     
@@ -45,6 +46,14 @@ mviewer.customLayers.arbresanciens= (function() {
         style: markerStyle,
     });
     
+    
+    dataLayer.once('prerender', function() {
+        console.log("COMPLEMENT ARBRES ANCIENS");
+        let features = dataLayer.getSource().getFeatures();
+        features.forEach(function(feature){
+            feature.set("complement", "Un arbre ancien a été planté il y a plus de " + age_ancien + " ans");
+        });
+    });
     
     return {
         layer: dataLayer,
