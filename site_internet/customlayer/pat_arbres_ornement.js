eac7a48f6dfa032a769f5581a39924b9ed6d9528
Cédric Briand <ce.briand@rennesmetropole.fr>
2022-10-21 08:25:23 +0200

    merge and squash from develop

---
+++ site_internet/customlayer/pat_arbres_ornement.js
@@ -0,0 +1,29 @@
+mviewer.customLayers.arbresornement= (function() {
+    let data_url = 'https://public.sig.rennesmetropole.fr/geoserver/wfs?service=WFS&version=1.0.0&request=GetFeature&typeNames=espub_esv:gev_aorn&outputFormat=application/json&srsName=EPSG:4326&CQL_FILTER=nom_commun IS NOT NULL';
+    let datacolor = '#bfbf30';
+    
+    function markerStyle() {
+        return [
+            new ol.style.Style({
+                image: new ol.style.Icon({
+                  color: datacolor, 
+                  crossOrigin: 'anonymous',
+                  src: 'apps/site_internet/customlayer/picture/rond_default.svg',
+                }),
+              })
+        ];
+    }
+    
+    let dataLayer = new ol.layer.Vector({
+        source: new ol.source.Vector({
+            url: data_url,
+            format: new ol.format.GeoJSON()
+        }),
+        style: markerStyle,
+    });
+    
+    return {
+        layer: dataLayer,
+    }
+}());
+
