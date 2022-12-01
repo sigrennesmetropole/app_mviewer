90937d9503a026cc106e016d311512cc9ad2e573
DAVID Benoît <b.david@mutu.local>
2022-12-01 10:50:46 +0100

    merge du center.js et du fixRightPanel.js en fixInfoPanels.js + gestion jolie de l'animation

--- public/addons/GUICustom/js/fixInfoPanels.js
+++ public/addons/GUICustom/js/fixInfoPanels.js
@@ -2,12 +2,17 @@
 * Déplace la toolbar à l'ouverture et à la fermeture du panneau de droite
 **/
 var $div = $("#right-panel");
+var _map = mviewer.getMap();
+var opendivwidth = 0;
+
 var observer = new MutationObserver(function(mutations) {
   mutations.forEach(function(mutation) {
     if (mutation.attributeName === "class") {
       var attributeValue = $(mutation.target).prop(mutation.attributeName);
       if(attributeValue === 'active'){
         setTimeout(function(){
+          opendivwidth=parseInt($div.css("width"),10);
+          decaleMap([opendivwidth/3,0]);
           var toolbarWidth = $div.outerWidth() + 5;
           var searchToolBar = toolbarWidth + 45;
           var searchToolBar2 = toolbarWidth + 92;
@@ -28,6 +33,7 @@ var observer = new MutationObserver(function(mutations) {
         $('#zoomtoolbar').animate({'right':'10px'});
         $('#toolstoolbar').animate({'right':'10px'});
         $('#backgroundlayerstoolbar-default').animate({'right':'10px'});
+        decaleMap([opendivwidth/3 * -1 ,0]);
       }
     }
   });
@@ -35,3 +41,16 @@ var observer = new MutationObserver(function(mutations) {
 observer.observe($div[0], {
   attributes: true
 });
+
+function decaleMap([x, y]) {
+  if (_map) {
+    var center = _map.getView().getCenter();
+    var resolution = _map.getView().getResolution();
+    _map.getView().animate({
+      center : [center[0] + x*resolution, center[1]+ y*resolution],
+      duration: 500,
+    });
+  } else {
+    console.warn('Erreur decalage');
+  }
+};
