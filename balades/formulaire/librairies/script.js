120abfd4001cbd47fe2694453f0b37bcab49bf68
ronanren <renouxpro@gmail.com>
2022-12-09 16:10:08 +0100

    amélioration formulaire

--- balades/formulaire/librairies/script.js
+++ balades/formulaire/librairies/script.js
@@ -1,4 +1,6 @@
 // Gestion du fichier GeoJSON fourni par l'utilisateur 
+var objetConvertPoints;
+var objetConvertLignes;
 document.getElementById('geojson').addEventListener('change', () => {
     const [file] = document.querySelector('#geojson').files;
     const reader = new FileReader();
@@ -7,125 +9,139 @@ document.getElementById('geojson').addEventListener('change', () => {
     }
 
     reader.addEventListener("load", () => {
+        document.querySelector(".filename").innerText = file.name;
         if (file.name.split('.').at(-1) == 'geojson') {
-            var [objetConvertPoints, objetConvertLignes] = conversionJSON(reader.result);
-            console.log(objetConvertPoints, objetConvertLignes);
+            try {
+                [objetConvertPoints, objetConvertLignes] = conversionJSON(reader.result);
+                console.log(objetConvertPoints, objetConvertLignes);
 
-            // Affichage du formulaire et de la carte
+                // Affichage du formulaire et de la carte
+                const elements = document.querySelectorAll(".boutonEnvoyer, .Panneauconfiguration, #map");
+                for (let element of elements) {
+                    if (element.classList.contains("hidden"))
+                        element.classList.remove("hidden");
+                }
+                document.getElementById('message').innerHTML = "";
+                map.updateSize();
+
+                // Mise en place des valeurs dans le formulaire
+                map.getView().setZoom(document.getElementById('zoomDefaut').value);
+
+                // Affichage des balades
+                var vectorLayerPoints = new ol.layer.Vector({
+                    source: new ol.source.Vector()
+                });
+                var vectorLayerBalades = new ol.layer.Vector({
+                    source: new ol.source.Vector()
+                });
+                const layers = map.getLayers().getArray().slice(1)
+                layers.forEach((layer) => map.removeLayer(layer))
+                objetConvertPoints.features.forEach(feature => {
+                    var point = new ol.Feature({
+                        geometry: new ol.geom.Point(feature.geometry.coordinates),
+                        values: feature.properties
+                    });
+                    var pointStyle = new ol.style.Style({
+                        image: new ol.style.Icon({
+                            anchor: [0.5, 1],
+                            src: "librairies/pin.svg",
+                            color: "black"
+                        })
+                    });
+                    point.setStyle(pointStyle);
+                    vectorLayerPoints.getSource().addFeature(point);
+                });
+                objetConvertLignes.features.forEach(feature => {
+                    var line = new ol.Feature({
+                        geometry: new ol.geom.LineString(feature.geometry.coordinates),
+                        values: feature.properties
+                    });
+                    var lineStyle = new ol.style.Style({
+                        stroke: new ol.style.Stroke({
+                            width: 3,
+                            color: feature.properties.couleur
+                        })
+                    });
+
+                    line.setStyle(lineStyle);
+                    vectorLayerBalades.getSource().addFeature(line);
+                });
+                map.addLayer(vectorLayerPoints);
+                map.addLayer(vectorLayerBalades)
+
+                // Attribut ID de chaque balade
+                const SelectAttributIdBalade = document.querySelector('#attributIdBalade');
+                while (SelectAttributIdBalade.firstChild) {
+                    SelectAttributIdBalade.removeChild(SelectAttributIdBalade.firstChild);
+                }
+                var optionSelectionnerListe = document.createElement("option");
+                optionSelectionnerListe.value = "";
+                optionSelectionnerListe.innerText = "Sélectionner dans la liste..";
+                SelectAttributIdBalade.appendChild(optionSelectionnerListe);
+                Object.keys(objetConvertLignes.features[0].properties).forEach(attribut => {
+                    const option = document.createElement("option");
+                    option.value = attribut;
+                    option.innerText = attribut;
+                    SelectAttributIdBalade.appendChild(option);
+                });
+
+                // Attribut ID de chaque point
+                const SelectAttributIdPoint = document.querySelector('#attributIdPoint');
+                while (SelectAttributIdPoint.firstChild) {
+                    SelectAttributIdPoint.removeChild(SelectAttributIdPoint.firstChild);
+                }
+                var optionSelectionnerListe = document.createElement("option");
+                optionSelectionnerListe.value = "";
+                optionSelectionnerListe.innerText = "Sélectionner dans la liste..";
+                SelectAttributIdPoint.appendChild(optionSelectionnerListe);
+                Object.keys(objetConvertPoints.features[0].properties).forEach(attribut => {
+                    const option = document.createElement("option");
+                    option.value = attribut;
+                    option.innerText = attribut;
+                    SelectAttributIdPoint.appendChild(option);
+                });
+
+                // Attribut Rang de chaque point
+                const SelectAttributRangPoint = document.querySelector('#attributRang');
+                while (SelectAttributRangPoint.firstChild) {
+                    SelectAttributRangPoint.removeChild(SelectAttributRangPoint.firstChild);
+                }
+                var optionSelectionnerListe = document.createElement("option");
+                optionSelectionnerListe.value = "";
+                optionSelectionnerListe.innerText = "Sélectionner dans la liste..";
+                SelectAttributRangPoint.appendChild(optionSelectionnerListe);
+                Object.keys(objetConvertPoints.features[0].properties).forEach(attribut => {
+                    const option = document.createElement("option");
+                    option.value = attribut;
+                    option.innerText = attribut;
+                    SelectAttributRangPoint.appendChild(option);
+                });
+
+                // Attribut de balade par défaut sélectionnée
+                const SelectAttributBaladeDefaut = document.querySelector('#baladeDefautSelectionnes');
+                while (SelectAttributBaladeDefaut.firstChild) {
+                    SelectAttributBaladeDefaut.removeChild(SelectAttributBaladeDefaut.firstChild);
+                }
+                var optionParametreOptionnel = document.createElement("option");
+                optionParametreOptionnel.value = "";
+                optionParametreOptionnel.innerText = "Paramètre optionnel..";
+                SelectAttributBaladeDefaut.appendChild(optionParametreOptionnel);
+                document.getElementById('message').style.display = 'none';
+            } catch (e) {
+                document.getElementById('message').style.display = 'block';
+                document.getElementById('message').innerHTML = "Le fichier n'est pas valide.";
+                const elements = document.querySelectorAll(".boutonEnvoyer, .Panneauconfiguration, #map");
+                for (let element of elements) {
+                    element.classList.add("hidden");
+                }
+            }
+        } else {
+            document.getElementById('message').style.display = 'block';
+            document.getElementById('message').innerHTML = "Le fichier n'est pas sous le bon format GeoJSON.";
             const elements = document.querySelectorAll(".boutonEnvoyer, .Panneauconfiguration, #map");
             for (let element of elements) {
-                if (element.classList.contains("hidden"))
-                    element.classList.remove("hidden");
+                element.classList.add("hidden");
             }
-            document.getElementById('message').innerHTML = "";
-            document.querySelector(".filename").innerText = file.name;
-            map.updateSize();
-
-            // Mise en place des valeurs dans le formulaire
-            map.getView().setZoom(document.getElementById('zoomDefaut').value);
-
-            // Affichage des balades
-            var vectorLayerPoints = new ol.layer.Vector({
-                source: new ol.source.Vector()
-            });
-            var vectorLayerBalades = new ol.layer.Vector({
-                source: new ol.source.Vector()
-            });
-            const layers = map.getLayers().getArray().slice(1)
-            layers.forEach((layer) => map.removeLayer(layer))
-            objetConvertPoints.features.forEach(feature => {
-                var point = new ol.Feature({
-                    geometry: new ol.geom.Point(feature.geometry.coordinates),
-                    values: feature.properties
-                });
-                var pointStyle = new ol.style.Style({
-                    image: new ol.style.Icon({
-                        anchor: [0.5, 1],
-                        src: "librairies/pin.svg",
-                        color: "black"
-                    })
-                });
-                point.setStyle(pointStyle);
-                vectorLayerPoints.getSource().addFeature(point);
-            });
-            objetConvertLignes.features.forEach(feature => {
-                var line = new ol.Feature({
-                    geometry: new ol.geom.LineString(feature.geometry.coordinates),
-                    values: feature.properties
-                });
-                var lineStyle = new ol.style.Style({
-                    stroke: new ol.style.Stroke({
-                        width: 3,
-                        color: feature.properties.couleur
-                    })
-                });
-
-                line.setStyle(lineStyle);
-                vectorLayerBalades.getSource().addFeature(line);
-            });
-            map.addLayer(vectorLayerPoints);
-            map.addLayer(vectorLayerBalades)
-
-            // Attribut ID de chaque balade
-            const SelectAttributIdBalade = document.querySelector('#attributIdBalade');
-            while (SelectAttributIdBalade.firstChild) {
-                SelectAttributIdBalade.removeChild(SelectAttributIdBalade.firstChild);
-            }
-            var optionSelectionnerListe = document.createElement("option");
-            optionSelectionnerListe.value = "";
-            optionSelectionnerListe.innerText = "Sélectionner dans la liste..";
-            SelectAttributIdBalade.appendChild(optionSelectionnerListe);
-            Object.keys(objetConvertLignes.features[0].properties).forEach(attribut => {
-                const option = document.createElement("option");
-                option.value = attribut;
-                option.innerText = attribut;
-                SelectAttributIdBalade.appendChild(option);
-            });
-
-            // Attribut ID de chaque point
-            const SelectAttributIdPoint = document.querySelector('#attributIdPoint');
-            while (SelectAttributIdPoint.firstChild) {
-                SelectAttributIdPoint.removeChild(SelectAttributIdPoint.firstChild);
-            }
-            var optionSelectionnerListe = document.createElement("option");
-            optionSelectionnerListe.value = "";
-            optionSelectionnerListe.innerText = "Sélectionner dans la liste..";
-            SelectAttributIdPoint.appendChild(optionSelectionnerListe);
-            Object.keys(objetConvertPoints.features[0].properties).forEach(attribut => {
-                const option = document.createElement("option");
-                option.value = attribut;
-                option.innerText = attribut;
-                SelectAttributIdPoint.appendChild(option);
-            });
-
-            // Attribut Rang de chaque point
-            const SelectAttributRangPoint = document.querySelector('#attributRang');
-            while (SelectAttributRangPoint.firstChild) {
-                SelectAttributRangPoint.removeChild(SelectAttributRangPoint.firstChild);
-            }
-            var optionSelectionnerListe = document.createElement("option");
-            optionSelectionnerListe.value = "";
-            optionSelectionnerListe.innerText = "Sélectionner dans la liste..";
-            SelectAttributRangPoint.appendChild(optionSelectionnerListe);
-            Object.keys(objetConvertPoints.features[0].properties).forEach(attribut => {
-                const option = document.createElement("option");
-                option.value = attribut;
-                option.innerText = attribut;
-                SelectAttributRangPoint.appendChild(option);
-            });
-
-            // Attribut de balade par défaut sélectionnée
-            const SelectAttributBaladeDefaut = document.querySelector('#baladeDefautSelectionnes');
-            while (SelectAttributBaladeDefaut.firstChild) {
-                SelectAttributBaladeDefaut.removeChild(SelectAttributBaladeDefaut.firstChild);
-            }
-            var optionParametreOptionnel = document.createElement("option");
-            optionParametreOptionnel.value = "";
-            optionParametreOptionnel.innerText = "Paramètre optionnel..";
-            SelectAttributBaladeDefaut.appendChild(optionParametreOptionnel);
-
-        } else {
-            document.getElementById('message').innerHTML = "Le fichier n'est pas sous le bon format GeoJSON.";
         }
     }, false);
 });
@@ -148,13 +164,19 @@ function conversionJSON(objetTexte) {
     objetJSON.features.forEach(feature => {
         if (feature.geometry.type == "Point") {
             feature.geometry.coordinates = convertCoordinates4326to3857(feature.geometry.coordinates[0], feature.geometry.coordinates[1]);
-            feature.properties.id = parseInt(feature.properties.id);
-            feature.properties.rang = parseInt(feature.properties.rang);
-            feature.properties.idbalade = parseInt(feature.properties.idbalade);
+            for (const [key, value] of Object.entries(feature.properties)) {
+                if (/^\d+$/.test(value)) {
+                    feature.properties[key] = parseInt(value);
+                }
+            }
             objetConvertPoints.features.push(feature);
         } else if (feature.geometry.type == "LineString") {
             feature.geometry.coordinates = feature.geometry.coordinates.map(coordinate => convertCoordinates4326to3857(coordinate[0], coordinate[1]));
-            feature.properties.id = parseInt(feature.properties.id);
+            for (const [key, value] of Object.entries(feature.properties)) {
+                if (/^\d+$/.test(value)) {
+                    feature.properties[key] = parseInt(value);
+                }
+            }
             objetConvertLignes.features.push(feature);
         }
 
@@ -226,6 +248,8 @@ document.querySelectorAll("#attributIdPoint, #attributIdBalade").forEach(element
                 });
                 feature.setStyle(pointStyle);
             });
+            couleurPointActif();
+            couleurBaladeDefaut();
         } catch (error) {
             vectorLayerPoints.getSource().forEachFeature(feature => {
                 // récuperer l'entité lineaire correspondante à la balade
@@ -260,3 +284,91 @@ document.querySelector("#attributIdBalade").addEventListener('change', (e) => {
         SelectAttributBaladeDefaut.appendChild(option);
     });
 });
+
+// Gestion de l'affichage des points non séléctionnés
+document.querySelector("#affichagePointNonSelect").addEventListener('change', (e) => {
+    const vectorLayerPoints = map.getLayers().getArray()[1];
+    if (e.target.value == "Oui") {
+        vectorLayerPoints.setVisible(true);
+    } else {
+        vectorLayerPoints.setVisible(false);
+    }
+});
+
+// Gestion de la couleur du point actif
+function couleurPointActif() {
+    const vectorLayerPoints = map.getLayers().getArray()[1];
+    vectorLayerPoints.getSource().getFeatures()[0].setStyle(new ol.style.Style({
+        image: new ol.style.Icon({
+            anchor: [0.5, 1],
+            src: "librairies/pin.svg",
+            color: document.querySelector("#couleurPointActif").value
+        })
+    })
+    );
+}
+document.querySelector("#couleurPointActif").addEventListener('change', (e) => {
+    couleurPointActif();
+});
+
+// Gestion de la couleur par défaut de la balade 
+function couleurBaladeDefaut() {
+    const vectorLayerBalades = map.getLayers().getArray()[2];
+    vectorLayerBalades.getSource().getFeatures()[0].setStyle(new ol.style.Style({
+        stroke: new ol.style.Stroke({
+            width: 3,
+            color: document.querySelector("#couleurBaladeDefaut").value
+        })
+    }));
+}
+document.querySelector("#couleurBaladeDefaut").addEventListener('change', (e) => {
+    couleurBaladeDefaut();
+})
+
+// Gestion du zoom par défaut d'une balade active
+document.querySelector("#zoomBalade").addEventListener('change', (e) => {
+    // Zoom sur un point de la balade
+    const vectorLayerPoints = map.getLayers().getArray()[1];
+    var featurePoint = vectorLayerPoints.getSource().getFeatures()[0];
+    var coord = featurePoint.getGeometry().getCoordinates();
+    var lastCenter = map.getView().getCenter();
+    var lastZoom = map.getView().getZoom();
+    map.getView().animate({ zoom: e.target.value, center: coord, duration: 1000 });
+    setTimeout(function () {
+        map.getView().animate({ zoom: lastZoom, center: lastCenter, duration: 1000 });
+    }, 3000);
+});
+
+// Gestion des fichiers de données 
+document.querySelector("#boutonEnvoyer").addEventListener('click', (e) => {
+    var form = document.querySelector("#form");
+    if (form.checkValidity()) {
+        document.querySelector("#confirmation-modal").classList.remove("hidden");
+    } else {
+        form.reportValidity()
+    }
+});
+
+// Gestion du modal de confirmation
+document.querySelector("#envoyerFormulaireConfirm").addEventListener('click', (e) => {
+    var form = document.querySelector("#form");
+    var date = new Date();
+    var uid = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + "_" + date.getHours() + "-" + date.getMinutes() + "-" + date.getSeconds() + "_" + date.getMilliseconds();
+    let fichiers = {};
+    fichiers["points_" + uid + ".geojson"] = objetConvertPoints;
+    fichiers["balades_" + uid + ".geojson"] = objetConvertLignes;
+
+    fichiers["param_" + uid + ".json"] = {
+        "carte": { "zoomPendantBalade": parseInt(form.elements["zoomBalade"].value) },
+        "balades": { "layer_balades": "balades", "id": form.elements["attributIdBalade"].value, "couleurBalades": "couleur", "defaultColor": form.elements["couleurBaladeDefaut"].value, "baladeParDefaut": form.elements["baladeDefautSelectionnes"].value },
+        "points": { "layer_points": "balades_points", "idBalade": form.elements["attributIdPoint"].value, "champRang": form.elements["attributRang"].value, "couleurPointActif": form.elements["couleurPointActif"].value, "pointsVisible": form.elements["affichagePointNonSelect"].value == "Oui" ? "true" : "false" }
+    };
+
+    fichiers["balades_" + uid + ".xml"] = "A faire"; // En JSON ? json to xml converter
+    console.log(fichiers);
+
+});
+
+document.querySelector("#annulerConfirm").addEventListener('click', (e) => {
+    document.querySelector("#confirmation-modal").classList.add("hidden");
+})
