4f7cda5c2a9185aa705939cceab5afe7b441d953
Cédric Briand <ce.briand@rennesmetropole.fr>
2023-01-04 12:10:47 +0100

    numresult : chargement asynchrone de l'extension

--- public/addons/GUICustom/js/numresultstooltip.js
+++ public/addons/GUICustom/js/numresultstooltip.js
@@ -1,30 +1,35 @@
-var showNbFeatures = configuration.getConfiguration().application.showClickNbItems !== "false";
+var showNbFeatures = false;
 var coordinate = _map.getView().getCenter();
 
 var openedInfoPanel = false;
 
-
-if (showNbFeatures){
-    $("#page-content-wrapper").prepend("<div id='popup-number-results'></div>");
-    var _popup = new ol.Overlay({ positioning: 'center', element: $("#popup-number-results")[0], stopEvent: false})
-    mviewer.getMap().addOverlay(_popup);
-    $("#popup-number-results").parent().hide();
-
-    _map.on('singleclick', function (evt) {
-        coordinate = evt.coordinate;
-        _popup.setPosition(coordinate);
+function _init() {
+    showNbFeatures = configuration.getConfiguration().application.showClickNbItems !== "false";
+    if (showNbFeatures){
+        $("#page-content-wrapper").prepend("<div id='popup-number-results'></div>");
+        var _popup = new ol.Overlay({ positioning: 'center', element: $("#popup-number-results")[0], stopEvent: false})
+        mviewer.getMap().addOverlay(_popup);
         $("#popup-number-results").parent().hide();
-    });
 
-    document.addEventListener('markerdisplayEvent', (e) => {
-        if (e.detail.display === 'none'){
-            $("#popup-number-results").parent().hide();
-        } else {
-            setTimeout(refreshResultsNumber ,250);
-        }
-    });
+        _map.on('singleclick', function (evt) {
+            console.log("CLIC DETECTÉ");
+            coordinate = evt.coordinate;
+            _popup.setPosition(coordinate);
+            refreshResultsNumber();
+        });
 
-    document.addEventListener('infopanel-ready', () => {refreshResultsNumber();});
+        document.addEventListener('markerdisplayEvent', (e) => {
+            console.log("MARQUEUR DETECTÉ");
+            if (e.detail.display === 'none'){
+                $("#popup-number-results").parent().hide();
+            } else {
+                setTimeout(refreshResultsNumber ,250);
+            }
+        });
+
+        document.addEventListener('infopanel-ready', () => {
+            console.log("PANNEAU DETECTÉ");refreshResultsNumber();});
+    }
 }
 
 function refreshResultsNumber(){
@@ -35,3 +40,6 @@ function refreshResultsNumber(){
         $("#popup-number-results").parent().hide();
     }
 }
+
+
+_map.once('postcompose', _init);
