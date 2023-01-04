5c70f97a8976087b9e8ba69b5155d35e1bbf7685
Cédric Briand <ce.briand@rennesmetropole.fr>
2023-01-04 09:09:23 +0100

    mviewer v3.9.1 + changement besoin patri arboré + gestionnaire events

--- public/addons/GUICustom/js/numresultstooltip.js
+++ public/addons/GUICustom/js/numresultstooltip.js
@@ -40,3 +40,45 @@ function refreshResultsNumber(){
 
 
 _map.once('postcompose', _init);
+var showNbFeatures = false;
+var coordinate = _map.getView().getCenter();
+
+var openedInfoPanel = false;
+
+function _init() {
+    showNbFeatures = configuration.getConfiguration().application.showClickNbItems !== "false";
+    if (showNbFeatures){
+        $("#page-content-wrapper").prepend("<div id='popup-number-results'></div>");
+        var _popup = new ol.Overlay({ positioning: 'center', element: $("#popup-number-results")[0], stopEvent: false})
+        mviewer.getMap().addOverlay(_popup);
+        $("#popup-number-results").parent().hide();
+
+        _map.on('singleclick', function (evt) {
+            coordinate = evt.coordinate;
+            _popup.setPosition(coordinate);
+            refreshResultsNumber();
+        });
+
+        document.addEventListener('markerdisplayEvent', (e) => {
+            if (e.detail.display === 'none'){
+                $("#popup-number-results").parent().hide();
+            } else {
+                setTimeout(refreshResultsNumber ,250);
+            }
+        });
+
+        document.addEventListener('infopanel-ready', () => {refreshResultsNumber();});
+    }
+}
+
+function refreshResultsNumber(){
+    if (showNbFeatures && info.getQueriedFeatures().length > 1) {
+        $("#popup-number-results").html(info.getQueriedFeatures().length + ' résultats');
+        $("#popup-number-results").parent().show();
+    } else {
+        $("#popup-number-results").parent().hide();
+    }
+}
+
+
+_map.once('postcompose', _init);
