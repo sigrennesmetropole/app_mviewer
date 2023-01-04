e3fe0001c663bc0f8f901184cbcc819bb95c49f7
Cédric Briand <ce.briand@rennesmetropole.fr>
2023-01-04 16:34:28 +0100

    Révision carte projets diffus

--- site_internet/customlayer/urba_diffus_projets.js
+++ site_internet/customlayer/urba_diffus_projets.js
@@ -1,107 +1,19 @@
 mviewer.customLayers.urbadiffus_en_projet= (function() {
-    const fillcolor='#FF9027';
-    const nb_logements_min=15;
+    const fillcolor='#3FB3CD';
+    const nb_logements_min=10;
     let data_url="https://public.sig.rennesmetropole.fr/geoserver/wfs?service=WFS&version=1.0.0&request=GetFeature&typeNames=app:tabou_v_oa_programme&outputFormat=application/json&srsName=EPSG:3857";
-    let filter = "commune='Rennes' AND nature = 'En diffus' AND diffusion_restreinte=false AND etape='En projet' AND (nb_logements >= " + nb_logements_min + " OR num_ads IS NOT NULL)";
+    let filter = "commune='Rennes' AND nature = 'En diffus' AND diffusion_restreinte=false AND etape='En projet' AND (nb_logements >= " + nb_logements_min + " OR nb_logements IS NULL)";
     //let filter = "commune='Rennes' AND nature = 'En diffus' AND diffusion_restreinte=false AND etape='En projet' AND (nb_logements >= " + nb_logements_min + ")";
     
     let complete_url = data_url + '&CQL_FILTER='+ encodeURIComponent(filter);
 
-    /**
-    * Requete http avec header (API S&O qui nécessite une clé en header)
-    **/
-    function ads_data(requestUrl, principal=false) {
-        return new Promise(resolve => {
-            $.ajax({
-                url: requestUrl,
-                context: document.body,
-            }).done(function (res) {
-                resolve({'response': res});
-            });
-        });
-    };
-    
-    /**
-    * Données agglomérées programmes/ADS
-    **/
-    function getADSData(){
-        let programmes = dataLayer.getSource().getFeatures();
-        let l_ads=[];
-        for (pg in programmes) {
-            pg_data = programmes[pg];
-            //si num_ads existe alors aller chercher les infos complémentaires des ADS + geom ==> faire cette action pour tous les cas
-            // Recherche du numads dans la table v_ads_ddc (liste de num ads pour un seul appel complémentaire ?)
-            if (pg_data.get('num_ads') != undefined && pg_data.get('num_ads').trim() != ''){
-                l_ads.push(pg_data.get('num_ads'));
-            } 
-        }
-        //on récupère les données ADS
-        let filter_ads = "nature_op='Construction' AND num_ads IN ('" + l_ads.join("','") + "')";
-        ads_data('https://public.sig.rennesmetropole.fr/geoserver/wfs?service=WFS&version=1.0.0&request=GetFeature&typeNames=app:tabou_v_ads_ddc&outputFormat=application/json&srsName=EPSG:3857' + '&CQL_FILTER='+ encodeURIComponent(filter_ads)).then(function(result_ads){
-             let grp_features = [];
-             for (feat in result_ads.response.features){
-                 grp_features[result_ads.response.features[feat].properties.num_ads] = [];
-                 // verifier le nombre de logements 
-                 if (result_ads.response.features[feat].properties.nb_logements > nb_logements_min){
-                     // besoin de garder shape + nb_logements + nb_niveau_max + aire_geo
-                     grp_features[result_ads.response.features[feat].properties.num_ads].push(result_ads.response.features[feat]);
-                 } 
-             }
-             for (ads in grp_features){
-                 if (grp_features[ads].length > 1){
-                    // si plusieurs entrées, fusionner le shape
-                    let nb_niv=0;
-                    let aire_totale=0;
-                    let nb_log = 0;
-                    let geom = new ol.geom.MultiPolygon();
-                    for (feat in grp_features[ads]) {
-                        if (nb_niv < grp_features[ads][feat].properties.nb_niv){ nb_niv=grp_features[ads][feat].properties.nb_niv;}
-                        if (nb_log < grp_features[ads][feat].properties.nb_logements){ nb_log=grp_features[ads][feat].properties.nb_logements;}
-                        aire_totale += grp_features[ads][feat].properties.aire_geo;
-                        geom.union(new ol.geom.MultiPolygon(grp_features[ads][feat].geometry.coordinates));
-                    }
-                    grp_features[ads][0].properties.nb_logements = nb_log;
-                    grp_features[ads][0].properties.nb_niv = nb_niv;
-                    grp_features[ads][0].properties.aire_geo = aire_totale;
-                    grp_features[ads][0].geom = geom;
-                 } else {
-                     grp_features[ads][0].geom = new ol.geom.MultiPolygon(grp_features[ads][0].geometry.coordinates);
-                 }
-                 //MAJ Layer feature
-                 let ft_data = grp_features[ads][0];
-                 updateFeatureData(ft_data.properties.num_ads, ft_data.properties.nb_logements, ft_data.properties.nb_niv, ft_data.properties.aire_geo, ft_data.geom);
-             }
-             cleanData();
-             dataLayer.getSource().changed();
-        });
-    }
-    
-    function updateFeatureData(numads, nb_log, niveau, aire_geo, shape){
-        let programmes = dataLayer.getSource().getFeatures();
-        dataLayer.getSource().forEachFeature((feature) => {
-            if (feature.get('num_ads') == numads) {
-                feature.set('nb_logements', nb_log);
-                feature.set('niveau', niveau);
-                feature.set('aire_geo', aire_geo);
-                feature.setGeometry(shape);
-            }
-        });
-    }
-    
-    function deleteFeature(numads){
-        dataLayer.getSource().forEachFeature((feature) => {
-            if (feature.get("num_ads")==numads){
-                dataLayer.getSource().removeFeature(feature);
-            }
-        });
-    }
     
     function cleanData(){
-        // suppression des programmes qui ont moins de 15 logements
+        // suppression des programmes qui n'ont pas de descriptif
         dataLayer.getSource().forEachFeature((feature) => {
-            if (feature.get("nb_logements")< nb_logements_min){
+            if (!feature.get("description") || feature.get("description") == 'undefined' || feature.get("description").replace(/ [\s\r\n]+/gm, "").trim() == '' ) {
                 dataLayer.getSource().removeFeature(feature);
-            }
+            } 
         });
         // mise à jour du style
         dataLayer.setStyle(
@@ -120,8 +32,18 @@ mviewer.customLayers.urbadiffus_en_projet= (function() {
     let dataLayer = new ol.layer.Vector({
         visible: false,
         source: new ol.source.Vector({
-            url: complete_url,
-            format: new ol.format.GeoJSON()
+            format: new ol.format.GeoJSON(),
+            loader: () => { // permet d'éviter le bug de features chargées en double 
+                fetch(complete_url)
+                    .then(r => r.json())
+                    .then(r => {
+                        // nettoie la layer
+                        mviewer.getLayer("urbadiffus_en_projet").layer.getSource().clear();
+                        // charge les features
+                        let features = dataLayer.getSource().getFormat().readFeatures(r)
+                        dataLayer.getSource().addFeatures(features);
+                    }).then(r => {cleanData();})
+            }
         }),
         style: new ol.style.Style({
                 fill:new ol.style.Fill({
@@ -130,10 +52,7 @@ mviewer.customLayers.urbadiffus_en_projet= (function() {
             }),
     });
     
-    dataLayer.getSource().once('change',() =>{
-        //console.log("PROJET - complétude données");
-        getADSData();
-    });
+
     
     return {
         layer: dataLayer,
