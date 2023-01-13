7e5a7c65cdcb47d0f3aab9e1746f94872c83b4b3
Ronan RENOUX <r.renoux@rennesmetropole.fr>
2023-01-13 14:57:23 +0100

    fix problème chemin PHPMailer

--- balades/formulaire/envoiMail.php
+++ balades/formulaire/envoiMail.php
@@ -4,9 +4,9 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require_once __DIR__ . '/phpmailer/Exception.php';
-require_once __DIR__ . '/phpmailer/PHPMailer.php';
-require_once __DIR__ . '/phpmailer/SMTP.php';
+require_once __DIR__ . '/PHPMailer/Exception.php';
+require_once __DIR__ . '/PHPMailer/PHPMailer.php';
+require_once __DIR__ . '/PHPMailer/SMTP.php';
 
 $data_post = file_get_contents("php://input");
 $data = json_decode($data_post, true);
