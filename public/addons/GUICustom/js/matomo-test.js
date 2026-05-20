f99dcda37f982761f1b810dda5a8d08a46beece3
spelhate <s.pelhate@rennesmetropole.fr>
2026-05-20 16:50:06 +0200

    Branchement Matomo

--- public/addons/GUICustom/js/matomo-test.js
+++ public/addons/GUICustom/js/matomo-test.js
@@ -3,14 +3,13 @@ try {
     /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
     _paq.push(['trackPageView']);
     _paq.push(['enableLinkTracking']);
-    (function() {
+    (function () {
         var u = "https://matomo.sig.rennesmetropole.fr/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        _paq.push(['setTrackerUrl', u + 'matomo.php']);
+        _paq.push(['setSiteId', '5']);
+        var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+        g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
     })();
 } catch (error) {
     console.log("Matomo : " + error);
 }
-
