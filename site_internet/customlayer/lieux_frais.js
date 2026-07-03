6320aaf70071acdea0026e81411ef3c56ac077ab
Alban Dauleu <a.dauleu@rennesmetropole.fr>
2026-07-03 14:55:51 +0200

    Ajout de la carte "Lieux frais"

---
+++ site_internet/customlayer/lieux_frais.js
@@ -0,0 +1,66 @@
+mviewer.customLayers.lieuxfrais = (function () {
+
+    var data = 'apps/site_internet/customlayer/data/lieux_frais.geojson';
+
+    function lieux_frais_Style_pct(feature) {
+
+        var lieuxFrais = feature.get("Lieux frais");
+        var climatise = feature.get("Climatisé");
+
+        // On n'affiche que les lieux frais
+        if (lieuxFrais !== "Oui" && lieuxFrais !== "Partiellement") {
+            return null;
+        }
+
+        var color;
+        var zIndex;
+
+        // Détermination de la catégorie
+        if (lieuxFrais === "Oui" && climatise === "Oui") {
+            color = "#BCE9FC";      // Vert
+            zIndex = 20;
+        }
+        else if (lieuxFrais === "Partiellement" && climatise === "Oui") {
+            color = "#D2B0EB";      // Orange
+            zIndex = 20;
+        }
+        else if (lieuxFrais === "Oui") {
+            color = "#5D82FC";      // Bleu
+            zIndex = 10;
+        }
+        else if (lieuxFrais === "Partiellement") {
+            color = "#9261EB";      // Violet
+            zIndex = 10;
+        }
+        else {
+            return null;
+        }
+
+        return new ol.style.Style({
+            zIndex: zIndex,
+            image: new ol.style.Circle({
+                radius: 7,
+                fill: new ol.style.Fill({
+                    color: color
+                }),
+                stroke: new ol.style.Stroke({
+                    color: "#333333",
+                    width: 1.5
+                })
+            })
+        });
+    }
+
+    var lieuxfraislayer = new ol.layer.Vector({
+        source: new ol.source.Vector({
+            url: data,
+            format: new ol.format.GeoJSON()
+        }),
+        style: lieux_frais_Style_pct
+    });
+
+    return {
+        layer: lieuxfraislayer
+    };
+
+}());
