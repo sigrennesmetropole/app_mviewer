fe83f620b3d536d85aca9855232182bfe839aae9
fl.grasland <fl.grasland@rennesmetropole.fr>
2026-07-03 16:44:19 +0200

    maj symbologie sans acces libre = non

--- site_internet/customlayer/lieux_frais.js
+++ site_internet/customlayer/lieux_frais.js
@@ -4,52 +4,58 @@ mviewer.customLayers.lieuxfrais = (function () {
 
     function lieux_frais_Style_pct(feature) {
 
-        var lieuxFrais = feature.get("Lieux frais");
-        var climatise = feature.get("Climatisé");
+    var lieuxFrais = feature.get("Lieux frais");
+    var climatise = feature.get("Climatisé");
+    var accesLibre = feature.get("Accès libre");
 
-        // On n'affiche que les lieux frais
-        if (lieuxFrais !== "Oui" && lieuxFrais !== "Partiellement") {
-            return null;
-        }
-
-        var color;
-        var zIndex;
-
-        // Détermination de la catégorie
-        if (lieuxFrais === "Oui" && climatise === "Oui") {
-            color = "#BCE9FC";      // Vert
-            zIndex = 20;
-        }
-        else if (lieuxFrais === "Partiellement" && climatise === "Oui") {
-            color = "#D2B0EB";      // Orange
-            zIndex = 20;
-        }
-        else if (lieuxFrais === "Oui") {
-            color = "#5D82FC";      // Bleu
-            zIndex = 10;
-        }
-        else if (lieuxFrais === "Partiellement") {
-            color = "#9261EB";      // Violet
-            zIndex = 10;
-        }
-        else {
-            return null;
-        }
-
-        return new ol.style.Style({
-            zIndex: zIndex,
-            image: new ol.style.Circle({
-                radius: 7,
-                fill: new ol.style.Fill({
-                    color: color
-                }),
-                stroke: new ol.style.Stroke({
-                    color: "#333333",
-                    width: 1.5
-                })
-            })
-        });
+    // On n'affiche que les lieux en accès libre
+    if (accesLibre !== "Oui") {
+        return null;
     }
+
+    // On n'affiche que les lieux frais
+    if (lieuxFrais !== "Oui" && lieuxFrais !== "Partiellement") {
+        return null;
+    }
+
+    var color;
+    var zIndex;
+
+    // Détermination de la catégorie
+    if (lieuxFrais === "Oui" && climatise === "Oui") {
+        color = "#BCE9FC";
+        zIndex = 20;
+    }
+    else if (lieuxFrais === "Partiellement" && climatise === "Oui") {
+        color = "#D2B0EB";
+        zIndex = 20;
+    }
+    else if (lieuxFrais === "Oui") {
+        color = "#5D82FC";
+        zIndex = 10;
+    }
+    else if (lieuxFrais === "Partiellement") {
+        color = "#9261EB";
+        zIndex = 10;
+    }
+    else {
+        return null;
+    }
+
+    return new ol.style.Style({
+        zIndex: zIndex,
+        image: new ol.style.Circle({
+            radius: 7,
+            fill: new ol.style.Fill({
+                color: color
+            }),
+            stroke: new ol.style.Stroke({
+                color: "#333333",
+                width: 1.5
+            })
+        })
+    });
+}
 
     var lieuxfraislayer = new ol.layer.Vector({
         source: new ol.source.Vector({
